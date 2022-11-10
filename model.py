af0256ddc7bdc823506782991fedd9bf57f656a0
Caio Jordan <caioj2906@gmail.com>
2022-11-10 15:30:56 +0100

    divide lr by 10 at epoch 128

--- model.py
+++ model.py
@@ -81,9 +81,9 @@ class ForestCoverModel(LightningModule):
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
-        #lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, [32, 64, 96], gamma=0.2)
+        lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, [128], gamma=0.1)
 
-        return [optimizer], []
+        return [optimizer], [lr_scheduler]
 
     def calc_metrics(self, logits, target):
         metrics = {}
