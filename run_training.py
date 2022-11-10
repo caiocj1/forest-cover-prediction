f9ae7498b8702d4c536ea534218b2f5080beb32d
Caio Jordan <caioj2906@gmail.com>
2022-11-10 15:01:44 +0100

    256 width, 128 epochs

--- run_training.py
+++ run_training.py
@@ -51,7 +51,7 @@ if __name__ == '__main__':
         # Trainer
         trainer = Trainer(accelerator='auto',
                           devices=1 if torch.cuda.is_available() else None,
-                          max_epochs=256,
+                          max_epochs=128,
                           val_check_interval=300,
                           callbacks=[model_ckpt, lr_monitor],
                           logger=logger)
