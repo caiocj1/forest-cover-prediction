d9e1e0bfdf41a89af066c0918bccdcf034dce51d
Caio Jordan <caioj2906@gmail.com>
2022-11-10 15:10:06 +0100

    multistep

--- model.py
+++ model.py
@@ -72,7 +72,7 @@ class ForestCoverModel(LightningModule):
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-2)
-        lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 32, gamma=0.2)
+        lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, [48, 96], gamma=0.1)
 
         opt = {
             'optimizer': optimizer,
